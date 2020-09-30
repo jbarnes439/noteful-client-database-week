ec5ecbbd096fe2e71d0a89e636d88a200739da46
Josh Barnes <JBarnes49@gmail.com>
2020-09-30 14:03:28 -0700

    just making sure we're up to date

--- src/config.js
+++ src/config.js
@@ -3,9 +3,16 @@ export default {
 }
 
 // const dev = {
-//   API_ENDPOINT: 'http://localhost:8000/api'
-// }
+//   API_ENDPOINT: 'http://localhost:8000/api',
+//   // API_KEY: 'example-api-key'
+// };
 
 // const prod = {
+//   API_ENDPOINT: 'https://git.heroku.com/whispering-falls-62438.git'
+// }
 
+// const config = process.env.NODE_ENV === 'production' ? prod : dev;
+
+// export default {
+//   ...config
 // }
